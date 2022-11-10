6a02479d65b6e9812618a492ef39b90c39fea9fd
Gökhan Koçmarlı <gokhankocmarli@gmail.com>
2022-11-11 02:58:07 +0300

    refactor: Remove unneccesary

--- functions.py
+++
@@ -1,7 +0,0 @@
-def dummy_function(a: int, b: int):
-    """This function adds a and b and returns its result."""
-    return a + b
-
-def second_dummy_function(name: str):
-    """This function returns a string instance with a greeting."""
-    return f"Hello {name}!"
